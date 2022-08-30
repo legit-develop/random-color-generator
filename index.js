e1977d8285248d921b3127224334b63f5d98b4bb
Sifiso Dhlamini <sb.dhlamini@gmail.com>
2022-08-30 07:02:14 +0200

    create a random color function

--- index.js
+++ index.js
@@ -5,3 +5,12 @@ for (let index = 0; index < 30; index++) {
   colorContainerEl.classList.add("color-container");
   containerEl.appendChild(colorContainerEl);
 }
+
+function randomColor() {
+  const chars = "01234567abcdef";
+  const colorCodeLength = 6;
+  let color = "";
+  for (let index = 0; index < colorCodeLength; index++) {
+    const randomNum = Math.random() * chars.length;
+  }
+}
